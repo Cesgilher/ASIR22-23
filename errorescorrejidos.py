7ed85664086e39f81da7448b5e53ccdec782269d
cesgilher <cesgilher@gmail.com>
2022-09-26 22:15:56 +0200

    sasa

--- errorescorrejidos.py
+++ errorescorrejidos.py
@@ -1,11 +1,17 @@
->>> type(1)
-<class 'int'>
+>>> type(4.5)
+<class 'float'>
 
->>> type(15)
-<class 'int'>
+>>> type(5.8)
+<class 'float'>
 
->>> type(0)
-<class 'int'>
+>>> type(2342423424.3)
+<class 'float'>
 
->>> type(-46)
-<class 'int'>
+>>> type(4.0)
+<class 'float'>
+
+>>> type(0.0)
+<class 'float'>
+
+>>> type(-23.5)
+<class 'float'>
